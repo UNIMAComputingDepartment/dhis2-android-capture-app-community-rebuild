8d9bda0f33f876e5f7b7de3828a49c8f8856fd9c
achisati <echisatijr@gmail.com>
2025-05-22 21:47:40 +0200

    Button component connected with relationshipViewModel

--- community/src/main/java/org/dhis2/community/ui/components/CollapsibleListButton.kt
+++ community/src/main/java/org/dhis2/community/ui/components/CollapsibleListButton.kt
@@ -1,36 +1,57 @@
 package org.dhis2.community.ui.components
 
-import androidx.compose.runtime.Composable
-import androidx.compose.foundation.*
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.*
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.*
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import org.dhis2.community.R
+import org.dhis2.community.relationships.CmtRelationshipViewModel
 
 @Composable
 fun CollapsibleListButton(
     tittle: String,
-    items: List<String>,
-    onItemClick: (String) -> Unit = {}
+    items: List<CmtRelationshipViewModel>,
+    onItemClick: (CmtRelationshipViewModel) -> Unit = {}
 ) {
     var expanded by remember { mutableStateOf(false) }
     var searchQuery by remember { mutableStateOf("") }
+
     val filteredItems = items.filter {
-        it.contains(searchQuery, ignoreCase = true)
+        it.primaryAttribute.contains(searchQuery, ignoreCase = true)
+
     }
 
     Column(
@@ -39,7 +60,6 @@ fun CollapsibleListButton(
             .fillMaxWidth()
             .background(Color(0xFFE0E0E0), shape = RoundedCornerShape(8.dp))
     ) {
-        // Header row
         Row(
             verticalAlignment = Alignment.CenterVertically,
             modifier = Modifier
@@ -77,7 +97,6 @@ fun CollapsibleListButton(
         }
 
         if (expanded) {
-
             if (items.size > 10) {
                 Box(
                     modifier = Modifier
@@ -102,7 +121,6 @@ fun CollapsibleListButton(
                         singleLine = true,
                         textStyle = TextStyle(fontSize = 12.sp, color = Color.Black),
                         modifier = Modifier
-                            //.fillMaxWidth()
                             .padding(horizontal = 20.dp, vertical = 5.dp),
                     )
                 }
@@ -115,12 +133,13 @@ fun CollapsibleListButton(
                     .verticalScroll(rememberScrollState())
                     .padding(horizontal = 10.dp)
             ) {
-                filteredItems.forEachIndexed { index, item ->
+                filteredItems.forEach { item ->
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
                         modifier = Modifier
                             .padding(start = 12.dp)
-                            .heightIn(max = 40.dp)
+                            .height(40.dp)
+                            //.heightIn(max = 40.dp, min = 40.dp)
                             .clickable { onItemClick(item) }
 
                     ) {
@@ -134,68 +153,68 @@ fun CollapsibleListButton(
                         Column(
                             modifier = Modifier
                                 .padding(4.dp)
-                                //.fillMaxWidth()
                         ) {
                             Text(
-                                text = "Ainafe Enock Chisati",
+                                text = item.primaryAttribute,
                                 fontSize = 14.sp,
                                 lineHeight = 7.sp,
 
                                 modifier = Modifier
-                                    //.fillMaxWidth()
-                                    //.fillMaxHeight()
-                                    //.clickable { onItemClick(item) }
                                     .padding(vertical = 0.dp)
+
                             )
 
+                            item.secondaryAttribute?.let {
+                                Text(
+                                    text = it,
+                                    fontSize = 10.sp,
+                                    lineHeight = 7.sp,
+
+                                    modifier = Modifier
+                                )
+                            }
+                        }
+                        Spacer(modifier = Modifier.width(6.dp))
+
+                        item.tertiaryAttribute?.let {
                             Text(
-                                text = "Female",
+                                text = it,
                                 fontSize = 12.sp,
-                                lineHeight = 7.sp,
+                                lineHeight = 10.sp,
 
                                 modifier = Modifier
-                                //.padding(start = 15.dp)
-                                //.fillMaxWidth()
-                                //.clickable { onItemClick(item) }
-                                //.padding(vertical = 8.dp)
-                            )
-
+                                    .padding(top = 6.dp)
+                                    .align(Alignment.Top)
+                                    //.weight(1f),
+                                )
                         }
-                        //Spacer(modifier = Modifier.width(10.dp))
-                        //Column {
-                            Text(
-                            text = "20 Years",
-                            fontSize = 9.sp,
-                            lineHeight = 7.sp,
 
+                        Spacer(modifier = Modifier.weight(1f))
+                        Icon(
+                            painter = painterResource(R.drawable.ic_navigate_next),
+                            contentDescription = null,
                             modifier = Modifier
+                                .size(20.dp)
+                                //.weight(1f)
+                                .padding(end = 7.dp)
                                 .fillMaxWidth()
-                                //.windowInsetsTopHeight()
-
-                            //.clickable { onItemClick(item) }
-                                .padding(top = 0.dp)
                         )
-                        //}
-
-
-
-                        //Spacer(modifier = Modifier.height(6.dp))
-
                     }
-                    
-
                 }
+
             }
         }
     }
 }
 
 
+/*
 @Preview(showBackground = true)
 @Composable
 fun CollapsibleButtonPreview() {
     val context = LocalContext.current
     val sampleItems = List(9) { "house#${it + 1}" }
+
 
     MaterialTheme {
         CollapsibleListButton(
@@ -207,3 +226,5 @@ fun CollapsibleButtonPreview() {
         )
     }
 }
+
+ */
--- community/src/main/java/org/dhis2/community/ui/previews/CollapsibleListButtonPreview.kt
+++ community/src/main/java/org/dhis2/community/ui/previews/CollapsibleListButtonPreview.kt
@@ -1,9 +1,10 @@
 package org.dhis2.community.ui.previews
 
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.material3.MaterialTheme
+import org.dhis2.community.relationships.CmtRelationshipViewModel
 import org.dhis2.community.ui.components.CollapsibleListButton
 
 
@@ -13,13 +14,35 @@ fun CollapsibleButtonPreview(){
     val context = LocalContext.current
     val sampleItems = List(11){"item ${it + 1}"}
 
+    /*val repo = remember { RelationshipRepository(D2Manager.getD2()) }
+    val tieAttributes = remember { repo.getRelatedTeis(
+        "",
+        "",
+        TODO()
+    ) }
+
+     */
+
+
+    val sampleClass = listOf(
+        CmtRelationshipViewModel("Jahnics Kotlin Code", "Female", "70 Years", "12345"),
+        CmtRelationshipViewModel("Witman Router", "null", "60 Years", "12345"),
+        CmtRelationshipViewModel("Russian Network", "Male", null, "null"),
+        CmtRelationshipViewModel("Haroon Switch", null, null, "12345"),
+        CmtRelationshipViewModel("James Kotlin", "Mphongo", null, "12345"),
+        CmtRelationshipViewModel("Witman Router", "Male", "30 Years", "12345"),
+        CmtRelationshipViewModel("Russian Network", "Male", null, "null"),
+        CmtRelationshipViewModel("Haroon Switch", null, null, "12345"),
+    )
+
     MaterialTheme{
         CollapsibleListButton(
             tittle = "Households",
-            items = sampleItems,
+            items = sampleClass,
             onItemClick = {//item ->
                 //Toast.makeText(context, "Clicked : $item", Toast.LENGTH_SHORT).show()
             }
         )
     }
 }
+
