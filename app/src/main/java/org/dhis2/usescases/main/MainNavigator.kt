55c815f88fc09f063c46461320740bb9e7ef17c5
Pacharo Simukonda <pachasim56@gmail.com>
2025-09-02 15:29:52 +0200

    fix: openTasks function in MainNavigator

--- app/src/main/java/org/dhis2/usescases/main/MainNavigator.kt
+++ app/src/main/java/org/dhis2/usescases/main/MainNavigator.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import org.dhis2.R
+import org.dhis2.community.tasking.ui.TaskingFragment
 import org.dhis2.usescases.about.AboutFragment
 import org.dhis2.usescases.main.program.ProgramFragment
 import org.dhis2.usescases.qrReader.QrReaderFragment
@@ -134,7 +135,7 @@ class MainNavigator(
 
     fun openTasks() {
         beginTransaction(
-            TasksFragment(),
+            TaskingFragment(),
             MainScreen.TASKS,
             useFadeInTransition = false
         )
